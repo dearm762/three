0426347c0247a5c2227071d2e3d58dc6ab25554a
dearm762 <misterfighter1990@gmail.com>
2024-03-16 13:25:18 +0600

    setting up camera

--- src/js/script.ts
+++ src/js/script.ts
@@ -2,8 +2,14 @@ import { WebGL1Renderer, Scene, PerspectiveCamera } from 'three'
 
 const renderer = new WebGL1Renderer()
 const scene = new Scene()
-const camera = new PerspectiveCamera()
+const camera = new PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
 
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera)
 
 document.body.appendChild(renderer.domElement)
