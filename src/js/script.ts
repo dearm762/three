c38620a5cf9e3d9ddfba6f617c6ec4610d9ddff5
dearm762 <misterfighter1990@gmail.com>
2024-03-17 15:03:36 +0600

    added my photo to bg plane

--- src/js/script.ts
+++ src/js/script.ts
@@ -9,12 +9,18 @@ import {
 	PlaneGeometry,
 	GridHelper,
 	DoubleSide,
+	SphereGeometry,
+	TextureLoader,
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import photo from '../img/me.jpg'
 
-const R = new WebGLRenderer({ antialias: true, alpha: true })
-R.setClearColor(0x1da1f2)
+const R = new WebGLRenderer()
+const textureLoader = new TextureLoader()
 const scene = new Scene()
+
+const texture = textureLoader.load(photo)
+
 const axesHelper = new AxesHelper(5)
 const camera = new PerspectiveCamera(
 	75,
@@ -23,17 +29,17 @@ const camera = new PerspectiveCamera(
 	1000
 )
 
-scene.add(axesHelper)
+// scene.add(axesHelper)
 camera.position.set(0, 0, 5)
 
 const boxGeometry = new BoxGeometry()
-const boxMaterial = new MeshBasicMaterial({ color: 'black' })
+const boxMaterial = new MeshBasicMaterial({ color: 0x000000 })
 const box = new Mesh(boxGeometry, boxMaterial)
-scene.add(box)
+// scene.add(box)
 
 const planeGeometry = new PlaneGeometry(30, 30)
 const planeMaterial = new MeshBasicMaterial({
-	color: 0xffffff,
+	map: texture,
 	side: DoubleSide,
 })
 const plane = new Mesh(planeGeometry, planeMaterial)
@@ -41,10 +47,17 @@ plane.rotation.x = -0.5 * Math.PI
 scene.add(plane)
 
 const gridHelper = new GridHelper(30)
-scene.add(gridHelper)
+// scene.add(gridHelper)
+
+const sphereGeometry = new SphereGeometry(4, 50, 50)
+const sphereMaterial = new MeshBasicMaterial({
+	color: 0x1da1f2,
+})
+const sphere = new Mesh(sphereGeometry, sphereMaterial)
+// scene.add(sphere)
 
 function animationOfBox() {
-	box.rotation.z += 0.1
+	box.rotation.x += 0.1
 	R.render(scene, camera)
 }
 
