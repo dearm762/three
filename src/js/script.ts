3372d95778a6f0681df821608f8bad3c6440f524
dearm762 <misterfighter1990@gmail.com>
2024-03-19 18:01:30 +0600

    deleted trash code

--- src/js/script.ts
+++ src/js/script.ts
@@ -3,25 +3,22 @@ import {
 	Scene,
 	PerspectiveCamera,
 	AxesHelper,
-	BoxGeometry,
 	MeshBasicMaterial,
 	Mesh,
 	PlaneGeometry,
 	GridHelper,
 	DoubleSide,
-	SphereGeometry,
 	TextureLoader,
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import photo from '../img/me.jpg'
 
-const R = new WebGLRenderer()
+const renderer = new WebGLRenderer()
 const textureLoader = new TextureLoader()
 const scene = new Scene()
-
 const texture = textureLoader.load(photo)
 
-const axesHelper = new AxesHelper(5)
+const axesHelper = new AxesHelper(25)
 const camera = new PerspectiveCamera(
 	75,
 	window.innerWidth / window.innerHeight,
@@ -29,13 +26,7 @@ const camera = new PerspectiveCamera(
 	1000
 )
 
-// scene.add(axesHelper)
 camera.position.set(0, 0, 50)
-
-const boxGeometry = new BoxGeometry()
-const boxMaterial = new MeshBasicMaterial({ color: 0x000000 })
-const box = new Mesh(boxGeometry, boxMaterial)
-// scene.add(box)
 
 const planeGeometry = new PlaneGeometry(30, 30)
 const planeMaterial = new MeshBasicMaterial({
@@ -43,27 +34,28 @@ const planeMaterial = new MeshBasicMaterial({
 	side: DoubleSide,
 })
 const plane = new Mesh(planeGeometry, planeMaterial)
-
 scene.add(plane)
 
 const gridHelper = new GridHelper(30)
-// scene.add(gridHelper)
+scene.add(gridHelper)
 
-const sphereGeometry = new SphereGeometry(4, 50, 50)
-const sphereMaterial = new MeshBasicMaterial({
-	color: 0x1da1f2,
+const orbitControls = new OrbitControls(camera, renderer.domElement)
+orbitControls.enableDamping = true
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+window.addEventListener('resize', () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
 })
-const sphere = new Mesh(sphereGeometry, sphereMaterial)
-// scene.add(sphere)
 
-function animationOfBox() {
-	box.rotation.x += 0.1
-	R.render(scene, camera)
+document.body.appendChild(renderer.domElement)
+
+function animate() {
+	requestAnimationFrame(animate)
+	orbitControls.update()
+	renderer.render(scene, camera)
 }
 
-const orbit = new OrbitControls(camera, R.domElement)
-
-R.setSize(window.innerWidth, window.innerHeight)
-R.setAnimationLoop(animationOfBox)
-
-document.body.appendChild(R.domElement)
+animate()
