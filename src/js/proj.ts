9e7a3ac26d1d90061cd614197d179dd21e191090
dearm762 <misterfighter1990@gmail.com>
2024-03-26 01:40:45 +0600

    created animate loop

--- src/js/proj.ts
+++ src/js/proj.ts
@@ -11,3 +11,12 @@ const camera = new T.PerspectiveCamera(
 const renderer = new T.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+
+
+function animate() {
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
+
+animate()
