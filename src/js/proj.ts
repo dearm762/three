3a99af1719aba745eb9e4aea04d63c59982aa629
dearm762 <misterfighter1990@gmail.com>
2024-03-26 01:48:54 +0600

    arrow up ready

--- src/js/proj.ts
+++ src/js/proj.ts
@@ -8,11 +8,24 @@ const camera = new T.PerspectiveCamera(
 	1000
 )
 
+camera.position.set(0, 0, 50)
+camera.lookAt(0, 0, 0)
+
 const renderer = new T.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const material = new T.LineBasicMaterial({ color: 0x0000ff })
 
+const points = []
+points.push(new T.Vector3(-10, 0, 0) as never)
+points.push(new T.Vector3(0, 10, 0) as never)
+points.push(new T.Vector3(10, 0, 0) as never)
+
+const geometry = new T.BufferGeometry().setFromPoints(points)
+
+const line = new T.Line(geometry, material)
+scene.add(line)
 
 function animate() {
 	requestAnimationFrame(animate)
