e9b956028e52436de8254fac870d5f922e30beac
dearm762 <misterfighter1990@gmail.com>
2024-03-26 01:20:53 +0600

    created new proj

---
+++ src/js/proj.ts
@@ -0,0 +1,13 @@
+import * as T from 'three'
+
+const scene = new T.Scene()
+const camera = new T.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
+
+const renderer = new T.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
